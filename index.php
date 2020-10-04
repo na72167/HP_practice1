109c024ad067f3d1386323223815107e811f58e8
yh72167 <yh.19972372@gmail.com>
2020-10-04 23:06:29 +0900

    [add]ヘッダー部分の追加

--- index.php
+++ index.php
@@ -3,17 +3,91 @@
 <head>
   <meta charset="UTF-8">
   <title>Project</title></title>
-  <link rel="stylesheet" href="root/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
+  <link rel="stylesheet" href="root/css/style.css">
 </head>
 <body>
   <header class="header">
-  ヘッダー
+    <div class="header__header-title">
+      BRANCHECHOCOLAT
+    </div>
+    <nav class="header__header-nav">
+      <ul>
+        <li class="header__header-navElement">SHOP</li>
+        <li class="header__header-navElement">CONCEPT</li>
+        <li class="header__header-navElement">CHEF</li>
+        <li class="header__header-navElement">JOURNAL</li>
+      </ul>
+    </nav>
+      <div class="header__header-userWrap">
+        <div class="header__header-users">LOGIN</div>
+        <div class="header__header-users">SIGNIN</div>
+      </div>
+      <!-- ログイン後表示 -->
+      <!-- <div>USERNAME</div>
+      <div>LOGOUT</div> -->
+      <div class="header__header-border"></div>
   </header>
-  <main class="main">
-  メインコンテンツ
-  </main>
-  <footer class="footer">
-  フッター
-  </footer>
+  <!-- 今後はmainタグで囲わずsectionタグで小分けにする。(BEMで書く意味が減る) -->
+  <section class="main">
+    <div class="main__border-wrap">
+      <div class="main__border-1"></div>
+      <div class="main__border-2"></div>
+      <span class="main__sihuku">#至福の正方形</span>
+      <span class="main__theme">HOW YOU CREATE <br>THE LIFE</span>
+    </div>
+    <div class="main">
+      <span>SCROLL</span>
+      <img href="#">
+    </div>
+  </section>
+    <!-- <span>
+      IF YOU KNOW HAPPINESS,CATCH UP EAT UP STEP OUT
+    </span>
+    <div>
+    BRANCHÉ＝繋ぐ・差し込む。
+    ”食のリテラシーを磨く”を掲げる
+    一流の料理人と、ローカル食材が出会い、
+    今の感性で仕上げられたBRANCHÉ CHOCOLATの
+    カレ・オ・ショコラ(#カレチョコ)です。
+    感覚の鋭いあなたに、新しいものに敏感なあの人に、
+    一口で出逢える食の「驚き」と「幸せ」を。
+    至福の正方形が、感性を一歩先まで繋ぎます。
+    </div>
+    <section>
+      <h4>RECENT NEWS</h4>
+      <div>
+        <li>リスト関係の表示</li>
+      </div>
+    </section>
+      <h1>FLAVORS</h1>
+      <div>商品一覧</div>
+      <span>VIEW ALL</span>
+    <section>
+    </section>
+      <div>
+      EVERY MORNING ∗WE OFFER  OUR  HANDS AND OURTHE ORIGIN OF
+HAPPINESS MINDS TO  MAKE EACH CHOCOLAT UNIQUE.
+      </div>
+    <section>
+      <span>INSTAGRAM</span>
+      <div>一覧を見る</div>
+    </section>
+    <section>
+      <div>新商品の情報はライン公式アカウントでも配信中です。</div>
+      <nav>
+      SHOP
+      CONCEPT
+      CHEF
+      JOURNAL
+      ご利用案内
+      CONTACT
+      プライバシー・ポリシー
+      特定商取引法に基づく表記
+      </nav>
+    </section> -->
+  <!-- <footer>
+    <span>Copyright © Marche Inc. All Rights Reserved</span>
+  </footer> -->
 </body>
 </html>
